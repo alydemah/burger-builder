73b81e72bfa20867ddeed32e9b93f24b5a140530
alydemah <alydemah@gmail.com>
2017-12-21 12:17:10 +0100

    Adding Build control

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,25 +4,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 //
-const burger = (props) => {
-    let transformedngredients = Object.keys(props.ingredients)
-    .map(igKey => {
-         return [...Array(props.ingredients[igKey])].map((_, i) => {
+const burger = ( props ) => {
+    let transformedIngredients = Object.keys( props.ingredients )
+    .map( igKey => {
+         return [...Array( props.ingredients[igKey] )].map( (_, i) => {
             return   <BurgerIngredient key={igKey + i} type={igKey} />;
-         }); // Now we have Array of two elements
+         }); 
     })
     .reduce((arr, el) => {
         return arr.concat(el)
     }, []);
-    if (transformedngredients.length === 0) {
-        transformedngredients = <p>Please start adding ingredients!</p>;
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
     }
     
     return (
         // 
       <div className={classes.Burger}>
         <BurgerIngredient type="bread-top" />
-        {transformedngredients}
+        {transformedIngredients}
         <BurgerIngredient type="bread-bottom" />
       </div>
     );
